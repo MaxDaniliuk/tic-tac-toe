0134f8331924bcac86c65ecfe4dc97d4ae5bb7c4
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2024-05-12 14:04:43 +0300

    Allow players play the game in the browser

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameboard = (function() {
     const board = [
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ];
     // Used to display the board in console. Won't be required after setting up the DOM.
     const displayBoard = (board) => {
@@ -22,11 +22,11 @@ const gameboard = (function() {
     
 })();
 
-//Contains the logic of the game
 const gameController = (function() {
     const board = gameboard.getBoard();
     const playerOne = 'Player One';
     const playerTwo = 'Player Two';
+    let gameFinished = false;
 
     const players = [
         {
@@ -40,89 +40,78 @@ const gameController = (function() {
     ];
 
     let activePlayer = players[0];
-    //const getActivePlayer = () => activePlayer;
 
-    //Switches players after each move
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
         console.log(`${activePlayer.name}'s turn!`);
     }
 
-    //Evaluates whether it is a tie or not that is based on empty cells
+    const hasGameFinished = () => gameFinished;
+
     const checkTie = () => {
         const flattenedBoard = [].concat(...board);
-        const numOfEmptyCells = flattenedBoard.filter((emptyString) => (emptyString === ' ')).length;
+        const numOfEmptyCells = flattenedBoard.filter((emptyString) => (emptyString === '')).length;
         if (numOfEmptyCells === 0) {
             console.log(`It's a tie!`)
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     const checkWin = () => {
-        if ((board[0][0] !== ' ' && board[0][0] === board[0][1] && board[0][1] === board[0][2]) || 
-            (board[1][0] !== ' ' && board[1][0] === board[1][1] && board[1][1] === board[1][2]) || 
-            (board[2][0] !== ' ' && board[2][0] === board[2][1] && board[2][1] === board[2][2]) || 
-            (board[0][0] !== ' ' && board[0][0] === board[1][0] && board[1][0] === board[2][0]) || 
-            (board[0][1] !== ' ' && board[0][1] === board[1][1] && board[1][1] === board[2][1]) || 
-            (board[0][2] !== ' ' && board[0][2] === board[1][2] && board[1][2] === board[2][2]) || 
-            (board[0][0] !== ' ' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) || 
-            (board[0][2] !== ' ' && board[0][2] === board[1][1] && board[1][1] === board[2][0])
+        if ((board[0][0] !== '' && board[0][0] === board[0][1] && board[0][1] === board[0][2]) || 
+            (board[1][0] !== '' && board[1][0] === board[1][1] && board[1][1] === board[1][2]) || 
+            (board[2][0] !== '' && board[2][0] === board[2][1] && board[2][1] === board[2][2]) || 
+            (board[0][0] !== '' && board[0][0] === board[1][0] && board[1][0] === board[2][0]) || 
+            (board[0][1] !== '' && board[0][1] === board[1][1] && board[1][1] === board[2][1]) || 
+            (board[0][2] !== '' && board[0][2] === board[1][2] && board[1][2] === board[2][2]) || 
+            (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) || 
+            (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0])
         ) { 
             console.log(`${activePlayer.name} won!!!`)
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
-    //Allows to selet a cell, however, does not prevent from ticking taken slots. 
-    //Won't be required after listening to the 'click' events
-    const tickCell = () => { 
-        let cell = Number(prompt(`${activePlayer.name} choose the cell 1 - 9: `));
-        let count = 1;
-        outer:
+    const tickCell = (cellIndex) => { 
+        let indexTracker = 0;
         for (let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
-                if (count === cell) {
-                     board[i][j] = activePlayer.marker;
-                     gameboard.displayBoard(board);
-                     break outer;
+                if (indexTracker === cellIndex) {
+                    board[i][j] = activePlayer.marker;
+                    gameboard.displayBoard(board);
+
+                    if (checkWin() || checkTie()) {
+                        return gameFinished = true;
+                    }
+
+                    switchPlayerTurn();
+                    return
                  }
-                 count++;
+                 indexTracker ++;
             }
         }
-        screenController.updateBoard();
     }
-
-    return { tickCell, switchPlayerTurn, checkWin, checkTie }
+    return { tickCell, hasGameFinished }
 })();
 
-const screenController = (function() {
-    const markerPlaces = [...document.querySelectorAll('.marker-place')]
-    const board = gameboard.getBoard();
-
-    const updateBoard = () => {
-        
-        ([].concat(...board)).forEach((boardValue, index) => {
-            if (boardValue !== ' ') {
-                markerPlaces[index].textContent = boardValue;
-            } 
-        })
+(function() {
+    const cells = document.querySelectorAll('.cell');
+    const updateBoard = (cellIndex) => {
+        [...cells][cellIndex].textContent = [].concat(...gameboard.getBoard())[cellIndex];
     };
 
-    return { updateBoard }
-})();
-
-//Runs the game 
-(function() { 
-    while(true) {
-        gameController.tickCell();
-        if (gameController.checkWin() || gameController.checkTie()) {
-            break;
+    const clickHandlerCell = (cellIndex) => (e) => {
+        if (!gameController.hasGameFinished()) {
+            if (e.target.textContent === '') {
+                gameController.tickCell(cellIndex);
+                updateBoard(cellIndex);
+            }
         }
-        gameController.switchPlayerTurn();
-    }
-})()
+    };
 
+    cells.forEach((cell, cellIndex) => {
+        cell.addEventListener('click', clickHandlerCell(cellIndex))
+    })
+})();
