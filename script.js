8fdeb733a5a8b7d1bad44f924695d97c38ba5c03
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2024-05-17 19:58:52 +0300

    Create a screenController that displays the result

--- script.js
+++ script.js
@@ -18,40 +18,45 @@ const gameboard = (function() {
 
     const getBoard = () => board;
 
-    return { getBoard, displayBoard }
+    const clearBoard = () => {
+        for (let row = 0; row < 3; row++) {
+            for (let column = 0; column < 3; column++) {
+                board[row][column] = '';
+            }
+        }
+    }
+
+    return { getBoard, clearBoard, displayBoard };
     
 })();
 
 const gameController = (function() {
     const board = gameboard.getBoard();
-    const playerOne = 'Player One';
-    const playerTwo = 'Player Two';
+    let result = null;
     let gameFinished = false;
 
     const players = [
         {
-           name: playerOne,
            marker: 'X', 
         },
         {
-            name: playerTwo,
             marker: '0',
         }
     ];
 
     let activePlayer = players[0];
     const getActivePlayer = () => activePlayer;
-    const resetPlayer = () => activePlayer = players[0];
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
-        console.log(`${activePlayer.name}'s turn!`);
+        console.log(`${activePlayer.marker}'s turn!`);
     }
 
     const hasGameFinished = () => gameFinished;
+    const getResult = () => result;
 
     const checkTie = () => {
-        const flattenedBoard = [].concat(...board);
-        const numOfEmptyCells = flattenedBoard.filter((emptyString) => (emptyString === '')).length;
+        const oneDimensionalBoard = [].concat(...board);
+        const numOfEmptyCells = oneDimensionalBoard.filter((emptyString) => (emptyString === '')).length;
         if (numOfEmptyCells === 0) {
             console.log(`It's a tie!`)
             return true;
@@ -69,7 +74,7 @@ const gameController = (function() {
             (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) || 
             (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0])
         ) { 
-            console.log(`${activePlayer.name} won!!!`)
+            console.log(`${activePlayer.marker} won!!!`)
             return true;
         }
         return false;
@@ -82,92 +87,99 @@ const gameController = (function() {
                 if (indexTracker === cellIndex) {
                     board[i][j] = activePlayer.marker;
                     gameboard.displayBoard(board);
-
-                    if (checkWin() || checkTie()) {
-                        return gameFinished = true;
+                    if (checkTie()) {
+                        result = "It's a tie!";
+                        gameFinished = true;
+                        return
                     }
-
+                    if (checkWin()) {
+                        result = `The winner is\n${getActivePlayer().marker}`;
+                        gameFinished = true;
+                        return;
+                    }
                     switchPlayerTurn();
-                    return
+                    return;
                  }
                  indexTracker ++;
             }
         }
-    }
+    } 
 
     const resetGame = () => {
+        gameboard.clearBoard();
+        result = null;
         gameFinished = false;
-        resetPlayer();
+        activePlayer = players[0];
     }
 
-    return { tickCell, hasGameFinished, checkTie, checkWin, getActivePlayer, resetGame }
+    return { tickCell, getActivePlayer, hasGameFinished, getResult, resetGame }
 })();
 
 (function() {
-    const cells = [...document.querySelectorAll('.cell')];
-    const buttons = document.querySelectorAll('.button');
-    const gameResultAnnouncement = document.querySelector('.game-result');
-    const markerTurn = document.querySelector('.marker-turn');
-    const gameEndModal = document.querySelector('.game-end-modal')
-    const board = gameboard.getBoard();
 
-    const updateBoard = (cellIndex) => {
-        cells[cellIndex].textContent = [].concat(...board)[cellIndex];
-        displayTurn();
-        if (gameController.hasGameFinished()) {
-            displayResult();
-        }
-    };
+    const screenContoller = {
+        init: function() {
+            this.cacheDom();
+            this.bindEvents();
+            this.displayTurn();
+        },
+        cacheDom: function() {
+            this.cells = document.querySelectorAll('.cell');
+            this.buttons = document.querySelectorAll('.button');
+            this.turn = document.querySelector('.turn');
+            this.dialogs = document.querySelectorAll('dialog');
+            this.resultAnnouncementDialog = document.querySelector('.game-end-modal');
 
-    const displayTurn = () => {
-        markerTurn.textContent = gameController.getActivePlayer().marker;
-    }
-    displayTurn();
+        },
+        bindEvents: function() {
+            this.cells.forEach((cell, cellIndex) => {
+                cell.addEventListener('click', this.markCell.bind(this,cellIndex))
+            })
+            this.buttons.forEach(button => {
+                button.addEventListener('click', this.identifyButton.bind(this))
+                // Buttons should: 1. Close the dialog; 2. Not dialog related buttons
+                // 3. And later may open the dialog.
+            })
+        },
 
-    const displayResult = () => {
-        if (gameController.checkTie()) {
-            gameResultAnnouncement.textContent = "It's a tie!";
-        }
-        if (gameController.checkWin()) {
-            gameResultAnnouncement.textContent = `The winner is: ${gameController.getActivePlayer().name} !!`;
-        }
-        gameEndModal.showModal();
-    }
-
-    const clickHandlerCell = (cellIndex) => (e) => {
-        if (!gameController.hasGameFinished()) {
+        identifyButton: function(e) {
+            if (e.target.parentNode.classList.contains("modal-wrapper")) {
+                this.dialogs.forEach(dialog => {
+                    this.clearScreen();
+                    dialog.close();
+                })
+                //buttons that are inside a dialog
+            }
+            //Otherwise you are dealing with other button not in the dialog
+        },
+        markCell: function(cellIndex, e) {
             if (e.target.textContent === '') {
+                e.target.textContent = gameController.getActivePlayer().marker;
                 gameController.tickCell(cellIndex);
-                updateBoard(cellIndex);
+                this.displayTurn();
             }
-        }
-    };
-
-    cells.forEach((cell, cellIndex) => {
-        cell.addEventListener('click', clickHandlerCell(cellIndex))
-    })
-
-    const clickHandlerButton = (e) => {
-        if (e.target.classList.contains("play-again")) {
-            playAgain();
-        }
-    };
-
-    const playAgain = () => {
-        let index = 0;
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                board[i][j] = '';
-                cells[index].textContent = '';
-                index++;
+            if (gameController.hasGameFinished()) {
+                this.resultAnnouncementDialog.showModal();
+                this.displayResult();
+                
             }
-        }
-        gameController.resetGame();
-        displayTurn();
-        gameEndModal.close(); 
-    } 
+        },
+        displayTurn: function() {
+            const mark = `${gameController.getActivePlayer().marker} `;
+            this.turn.textContent = mark;
+        },
+        displayResult: function() {
+            document.querySelector('.game-result').textContent = gameController.getResult();
+        },
 
-    buttons.forEach((button) => {
-        button.addEventListener('click', clickHandlerButton)
-    })
+        clearScreen: function() {
+            gameController.resetGame();
+            this.cells.forEach(cell => {
+                cell.textContent = '';
+            })
+            this.displayTurn();
+        }
+    }
+
+    screenContoller.init();
 })();
